73d860ff7e44d926cb9292c3b2577d61d51384e1
rastislav <rabvoc@gmail.com>
2023-10-15 11:59:20 +0200

    add font and score feature

--- Scene2.js
+++ Scene2.js
@@ -4,13 +4,26 @@ class Scene2 extends Phaser.Scene {
   }
 
   create() {
-
-    this.background = this.add.tileSprite(0, 0, config.width, config.height , "background");
+    this.background = this.add.tileSprite(
+      0,
+      0,
+      config.width,
+      config.height,
+      "background"
+    );
     this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
+    this.ship1 = this.add.sprite(
+      config.width / 2 - 50,
+      config.height / 2,
+      "ship"
+    );
     this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
-    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");
+    this.ship3 = this.add.sprite(
+      config.width / 2 + 50,
+      config.height / 2,
+      "ship3"
+    );
 
     this.enemies = this.physics.add.group();
     this.enemies.add(this.ship1);
@@ -25,22 +38,16 @@ class Scene2 extends Phaser.Scene {
     this.ship2.setInteractive();
     this.ship3.setInteractive();
 
-    this.input.on('gameobjectdown', this.destroyShip, this);
-
-    this.add.text(20, 20, "Playing game", {
-      font: "25px Arial",
-      fill: "yellow"
-    });
+    this.input.on("gameobjectdown", this.destroyShip, this);
 
     this.physics.world.setBoundsCollision();
 
     this.powerUps = this.physics.add.group();
 
-
     for (var i = 0; i < gameSettings.maxPowerups; i++) {
       var powerUp = this.physics.add.sprite(16, 16, "power-up");
       this.powerUps.add(powerUp);
-       powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
+      powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
 
       if (Math.random() > 0.5) {
         powerUp.play("red");
@@ -51,78 +58,105 @@ class Scene2 extends Phaser.Scene {
       powerUp.setVelocity(gameSettings.powerUpVel, gameSettings.powerUpVel);
       powerUp.setCollideWorldBounds(true);
       powerUp.setBounce(1);
-
     }
 
-
-    this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
+    this.player = this.physics.add.sprite(
+      config.width / 2 - 8,
+      config.height - 64,
+      "player"
+    );
     this.player.play("thrust");
     this.cursorKeys = this.input.keyboard.createCursorKeys();
     this.player.setCollideWorldBounds(true);
 
-
-
-    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.spacebar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
 
     this.projectiles = this.add.group();
 
-    this.physics.add.collider(this.projectiles, this.powerUps, function(projectile,powerUp){
-      projectile.destroy();
-    }) 
+    this.physics.add.collider(
+      this.projectiles,
+      this.powerUps,
+      function (projectile, powerUp) {
+        projectile.destroy();
+      }
+    );
 
-    this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
-    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
-    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+    this.physics.add.overlap(
+      this.player,
+      this.powerUps,
+      this.pickPowerUp,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.player,
+      this.enemies,
+      this.hurtPlayer,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.projectiles,
+      this.enemies,
+      this.hitEnemy,
+      null,
+      this
+    );
 
+    var graphics = this.add.graphics();
+    graphics.fillStyle(0x000000, 1);
+    graphics.beginPath();
+    graphics.moveTo(0, 0);
+    graphics.lineTo(config.width, 0);
+    graphics.lineTo(config.width, 20);
+    graphics.lineTo(0, 20);
+    graphics.lineTo(0, 0);
+    graphics.closePath();
+    graphics.fillPath();
 
+    this.score = 0;
+    this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE ", 18);
   }
 
   update() {
-
     this.moveShip(this.ship1, 1);
     this.moveShip(this.ship2, 2);
     this.moveShip(this.ship3, 3);
 
     this.background.tilePositionY -= 0.5;
 
-
     this.movePlayerManager();
 
-
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
-        this.shootBeam();
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      this.shootBeam();
     }
-    for(var i = 0; i < this.projectiles.getChildren().length; i++){
+    for (var i = 0; i < this.projectiles.getChildren().length; i++) {
       var beam = this.projectiles.getChildren()[i];
       beam.update();
     }
-
-
   }
 
-  shootBeam(){
+  shootBeam() {
     var beam = new Beam(this);
   }
 
-
-  movePlayerManager(){
-
+  movePlayerManager() {
     this.player.setVelocity(0);
 
-    if(this.cursorKeys.left.isDown){
+    if (this.cursorKeys.left.isDown) {
       this.player.setVelocityX(-gameSettings.playerSpeed);
-    }else if(this.cursorKeys.right.isDown){
+    } else if (this.cursorKeys.right.isDown) {
       this.player.setVelocityX(gameSettings.playerSpeed);
     }
 
-    if(this.cursorKeys.up.isDown){
+    if (this.cursorKeys.up.isDown) {
       this.player.setVelocityY(-gameSettings.playerSpeed);
-    }else if(this.cursorKeys.down.isDown){
+    } else if (this.cursorKeys.down.isDown) {
       this.player.setVelocityY(gameSettings.playerSpeed);
     }
   }
-
-
 
   moveShip(ship, speed) {
     ship.y += speed;
@@ -142,18 +176,29 @@ class Scene2 extends Phaser.Scene {
     gameObject.play("explode");
   }
 
-  pickPowerUp(player, powerUp){
+  pickPowerUp(player, powerUp) {
     powerUp.disableBody(true, true);
   }
 
-  hurtPlayer(player, enemy){
+  hurtPlayer(player, enemy) {
     this.resetShipPos(enemy);
-    player.x = config.width / 2-8;
+    player.x = config.width / 2 - 8;
     player.y = config.height - 64;
   }
 
   hitEnemy(projectile, enemy) {
     projectile.destroy();
     this.resetShipPos(enemy);
+    this.score += 15;
+    var scoreFormated = this.zeroPad(this.score, 6);
+    this.scoreLabel.text = "SCORE " + scoreFormated;
+  }
+
+  zeroPad(number, size) {
+    var stringNumber = String(number);
+    while (stringNumber.length < (size || 2)) {
+      stringNumber = "0" + stringNumber;
+    }
+    return stringNumber;
   }
 }
