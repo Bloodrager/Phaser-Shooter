3d0d6eb2401978765bb35fd6aed709d4ba09f470
rastislav <rabvoc@gmail.com>
2023-10-15 11:05:13 +0200

    Implement shooting

--- Scene1.js
+++ Scene1.js
@@ -3,31 +3,36 @@ class Scene1 extends Phaser.Scene {
     super("bootGame");
   }
 
-  preload() {
-    this.load.image("background", "assets/background.png");
-    this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
+  preload(){
+    this.load.image("background", "assets/images/background.png");
+    //
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
       frameWidth: 16,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
       frameWidth: 32,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
       frameWidth: 32,
-      frameHeight: 16,
+      frameHeight: 32
     });
-    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
       frameWidth: 16,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png", {
+    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
       frameWidth: 16,
-      frameHeight: 16,
+      frameHeight: 16
     });
-    this.load.spritesheet("player", "assets/spritesheets/player.png", {
+    this.load.spritesheet("player", "assets/spritesheets/player.png",{
       frameWidth: 16,
-      frameHeight: 24,
+      frameHeight: 24
+    });
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
+      frameWidth: 16,
+      frameHeight: 16
     });
   }
 
@@ -36,64 +41,61 @@ class Scene1 extends Phaser.Scene {
     this.scene.start("playGame");
 
     this.anims.create({
-      key: "ship_anim",
+      key: "ship1_anim",
       frames: this.anims.generateFrameNumbers("ship"),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
     });
-
     this.anims.create({
       key: "ship2_anim",
       frames: this.anims.generateFrameNumbers("ship2"),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
     });
-
     this.anims.create({
       key: "ship3_anim",
       frames: this.anims.generateFrameNumbers("ship3"),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
     });
-
     this.anims.create({
-      key: "explode_anim",
+      key: "explode",
       frames: this.anims.generateFrameNumbers("explosion"),
       frameRate: 20,
       repeat: 0,
-      hideOnComplete: true,
+      hideOnComplete: true
     });
 
     this.anims.create({
       key: "red",
       frames: this.anims.generateFrameNumbers("power-up", {
         start: 0,
-        end: 1,
+        end: 1
       }),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
     });
-
     this.anims.create({
       key: "gray",
       frames: this.anims.generateFrameNumbers("power-up", {
         start: 2,
-        end: 3,
+        end: 3
       }),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
     });
-
     this.anims.create({
-      key: "thrust_anim",
+      key: "thrust",
       frames: this.anims.generateFrameNumbers("player"),
       frameRate: 20,
-      repeat: -1,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "beam_anim",
+      frames: this.anims.generateFrameNumbers("beam"),
+      frameRate: 20,
+      repeat: -1
     });
 
-    this.add.text(20, 20, "Playing game", {
-      font: "25px Aria",
-      fill: "yellow",
-    });
   }
 }
